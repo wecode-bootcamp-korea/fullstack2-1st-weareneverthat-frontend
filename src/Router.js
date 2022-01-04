34aeef965ece5b25b6c7c77edd5c3150aebe49b7
guminki <rnalsrl93@gmail.com>
2022-01-04 15:53:57 +0900

    pull

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-<<<<<<< HEAD:src/Routes.js
-=======
 import ProductList from './pages/productList';
 
->>>>>>> develop:src/Router.js
 function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-<<<<<<< HEAD:src/Routes.js
-				<></>
-=======
 				<Route path="/products" element={<ProductList />} />
->>>>>>> develop:src/Router.js
 			</Routes>
 		</BrowserRouter>
 	);
