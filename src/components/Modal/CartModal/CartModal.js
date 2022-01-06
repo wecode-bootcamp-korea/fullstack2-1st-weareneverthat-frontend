674091519a7aa1c8d0bf42dadde53069868c87b4
guminki <rnalsrl93@gmail.com>
2022-01-06 13:31:43 +0900

    cart 구현

--- src/components/Modal/CartModal/CartModal.js
+++ src/components/Modal/CartModal/CartModal.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import CartCard from '../../CartCard/CartCard';
 import './CartModal.scss';
 
-function CartModal(props) {
+function CartModal({ cartClass, closeCart }) {
 	const [carts, setCarts] = useState({});
 	useEffect(() => {
 		fetch('http://localhost:3000/data/cartMock.json', {
@@ -17,57 +18,63 @@ function CartModal(props) {
 			});
 	}, []);
 
-	// const subTotal = () => {
-	// 	let subTotal = 0;
-	// 	const priceArr = carts.list && carts.list.map(el => el.discountPrice);
-	// 	console.log(priceArr);
-	// 	for (let i = 0; i < priceArr.length; i++) {
-	// 		subTotal += priceArr[i];
-	// 	}
-	// 	return subTotal;
-	// };
+	const subTotal = () => {
+		let subTotal = 0;
+		const priceArr = carts.list.map(el => el.discountPrice);
+		console.log(priceArr);
+		for (let i = 0; i < priceArr.length; i++) {
+			subTotal += priceArr[i];
+		}
+		return subTotal;
+	};
 
-	// const result = subTotal();
+	const result = carts.list && subTotal();
 
 	return (
-		<div className={props.cartClass}>
+		<div className={cartClass}>
+			{carts.list && (
+				<section className="summary">
+					<section>ORDER SUMMARY</section>
+					<section className="closeBtn" onClick={closeCart}>
+						CLOSE
+					</section>
+				</section>
+			)}
 			<section className="wrapper">
-				{carts.list &&
-					carts.list.map(el => {
+				{carts.list ? (
+					carts.list.map((el, index) => {
 						return (
-							<>
-								<section className="productCard">
-									<div className="mainImage">
-										<img src={el.imageUrl} width="100px" height="100px" />
-									</div>
-									<div className="Info">
-										<section className="productName">
-											<span>{el.name}</span>
-											<span>{el.color}</span>
-											<span>size {el.size}</span>
-										</section>
-										<section className="productPrice">
-											<section>{el.price}</section>
-											<section>{el.discountPrice}</section>
-										</section>
-									</div>
-								</section>
-							</>
+							<CartCard
+								key={index}
+								imageUrl={el.imageUrl}
+								name={el.name}
+								color={el.color}
+								size={el.size}
+								price={el.price}
+								discountPrice={el.discountPrice}
+								closeCart={closeCart}
+							/>
 						);
-					})}
+					})
+				) : (
+					<section className="empty">
+						<p>Your cart is empty</p>
+						<p>
+							<Link to="/products">CONTINUE SHOPPING</Link>
+						</p>
+					</section>
+				)}
 			</section>
-
-			<section className="total">
-				<div>SUBTOTAL</div>
-				{/* <div>{result}</div> */}
-			</section>
-			<section className="checkoutBtn">버튼</section>
-			{/* <section>
-				<p>Your cart is empty</p>
-				<p>
-					<Link to="/products">CONTINUE SHOPPING</Link>
-				</p>
-			</section> */}
+			{carts.list && (
+				<section className="cartFooter">
+					<section className="total">
+						<div>SUBTOTAL</div>
+						<div>₩{result}</div>
+					</section>
+					<button className="checkoutBtn">CHECKOUT</button>
+					<section className="subInfo">배송비 및 쿠폰 적용은 결제 단계이서 적용됩니다.</section>
+				</section>
+			)}
 		</div>
 	);
 }
