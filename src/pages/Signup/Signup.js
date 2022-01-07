939c7572cb8a2d7f74b8f8d2f0fade55dd38af0d
민하늘 <onlynforever@gmail.com>
2022-01-07 13:36:54 +0900

    구현 완료

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -62,77 +62,73 @@ function Signup() {
 	};
 
 	return (
-		<>
+		<div className="sign">
 			<Topnav />
-			<div className="sign">
-				<div className="Signup">
-					<div className="pageInfo">
-						<div className="accountBox" onClick={goToLogin}>
-							Account
-						</div>
-						<div className="createAccountBox">Create&nbsp;account</div>
+			<div className="Signup">
+				<div className="pageInfo">
+					<div className="accountBox" onClick={goToLogin}>
+						Account
 					</div>
-					<section className="signUpWrapper" style={{ visibility: formVisibility }}>
-						<form className="inputBox">
-							<section className="nameInput">
-								<div>이름</div>
-								<input
-									type="text"
-									placeholder="이름"
-									value={nameValue}
-									onChange={handleNameInput}
-								></input>
-							</section>
-							<section className="emailInput">
-								<div>이메일</div>
-								<input
-									type="text"
-									placeholder="이메일"
-									value={emailValue}
-									onChange={handleEmailInput}
-								></input>
-							</section>
-							<section className="pwInput">
-								<div>비밀번호</div>
-								<input
-									type="password"
-									placeholder="비밀번호"
-									value={pwValue}
-									onChange={handlePwInput}
-								></input>
-							</section>
-						</form>
-						<section className="signupBtn">
-							<button onClick={signupLogic}>CREATE ACCOUNT</button>
-						</section>
-						<section className="returnLogin" onClick={goToLogin}>
-							<div>로그인으로 돌아가기</div>
-						</section>
-						<section className="alert" style={{ visibility: alertVisibility }}>
-							<div>
-								형식이 잘못됐습니다.
-								<br />
-								<br />
-								이메일 형식의 아이디와 8자 이상의 영문 대소문자, 숫자, 특수문자가 혼용된 비밀번호를
-								사용해주세요.
-							</div>
-						</section>
-					</section>
-					<section className="successCreate" style={{ visibility: successVisibility }}>
-						회원가입이 완료되었습니다!
-						<br />
-						<br />
-						로그인 후 이용 바랍니다.
-						<br />
-						<br />
-						weareneverthat®
-					</section>
+					<div className="createAccountBox">Create&nbsp;account</div>
 				</div>
+				<section className="signUpWrapper" style={{ visibility: formVisibility }}>
+					<form className="inputBox">
+						<section className="nameInput">
+							<div>이름</div>
+							<input
+								type="text"
+								placeholder="이름"
+								value={nameValue}
+								onChange={handleNameInput}
+							></input>
+						</section>
+						<section className="emailInput">
+							<div>이메일</div>
+							<input
+								type="text"
+								placeholder="이메일"
+								value={emailValue}
+								onChange={handleEmailInput}
+							></input>
+						</section>
+						<section className="pwInput">
+							<div>비밀번호</div>
+							<input
+								type="password"
+								placeholder="비밀번호"
+								value={pwValue}
+								onChange={handlePwInput}
+							></input>
+						</section>
+					</form>
+					<section className="signupBtn">
+						<button onClick={signupLogic}>CREATE ACCOUNT</button>
+					</section>
+					<section className="returnLogin" onClick={goToLogin}>
+						<div>로그인으로 돌아가기</div>
+					</section>
+					<section className="alert" style={{ visibility: alertVisibility }}>
+						<div>
+							형식이 잘못됐습니다.
+							<br />
+							<br />
+							이메일 형식의 아이디와 8자 이상의 영문 대소문자, 숫자, 특수문자가 혼용된 비밀번호를
+							사용해주세요.
+						</div>
+					</section>
+				</section>
+				<section className="successCreate" style={{ visibility: successVisibility }}>
+					회원가입이 완료되었습니다!
+					<br />
+					<br />
+					로그인 후 이용 바랍니다.
+					<br />
+					<br />
+					weareneverthat®
+				</section>
 			</div>
-			<div className="footer">
-				<Footer />
-			</div>
-		</>
+			<Footer />
+		</div>
 	);
 }
 
